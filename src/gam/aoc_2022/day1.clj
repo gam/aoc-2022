301ffca6bab61fe86732d498ad4f897bfd47ec24
Geir Amdal <geir.amdal@nagarro.com>
2022-12-02 22:08:26 +0100

    Cleanup of day 1 solution

--- src/gam/aoc_2022/day1.clj
+++ src/gam/aoc_2022/day1.clj
@@ -3,21 +3,8 @@
             [gam.aoc :as aoc]
             [clojure.test :refer [deftest is testing]]))
 
-
-;; Evaluate the following expression to open today's puzzle.
-#_(aoc/open-puzzle-page 1)
-
-;; Evaluate the following expression to retrieve and store today's puzzle input.
-#_(aoc/download-puzzle-input 1)
-
 (def demo-input (aoc/fetch-data "demo-day1.txt"))
 (def real-input (aoc/fetch-data "puzzle-day1.txt"))
-(apply + 
-       (take 3 
-             (sort > 
-                   (map (comp #(apply + %) #(map aoc/->int %) str/split-lines) 
-                        (str/split real-input #"\n\n"))))))
-
 
 (defn ->elves [input]
   (str/split input #"\n\n"))
@@ -46,18 +33,9 @@
 (deftest aoc-2021.day1
   (testing solve-1
     (is (= (solve-1 demo-input) 24000))
-    (is (= (solve-1 real-input) 45000)))
+    (is (= (solve-1 real-input) 69289)))
   (testing solve-2
     (is (= (solve-2 demo-input) 45000))
-    (is (= (solve-2 real-input) 1248))))
+    (is (= (solve-2 real-input) 205615))))
 
 (clojure.test/run-tests)
-
-#_demo-input
-(parse-elf "1000\n2000\n3000")
-
-#_(solve-2 demo-input)
-
-(aoc/->int "1000")
-#_elves
-
